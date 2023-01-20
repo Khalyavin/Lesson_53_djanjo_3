cec6b2c6f901e324eb21d485319b52e1602194b1
Khalyavin <sts2004@gmail.com>
2023-01-20 17:09:26 +0700

    Task 3 ready

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    photo = models.ImageField(upload_to='images/', **NULLABLE)
+    category = models.ForeignKey('catalog.Category', on_delete=models.CASCADE)
+    price = models.FloatField(verbose_name='Цена')
+    date_org = models.DateField(null=True)
+    date_fill = models.DateField(null=True)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', 'description', ]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name', ]
+
+    def __str__(self):
+        return self.name
