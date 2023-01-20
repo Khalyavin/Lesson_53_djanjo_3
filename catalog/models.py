d9c8b31fe55009c46881b70522b03e1167b29402
Khalyavin <sts2004@gmail.com>
2023-01-20 23:37:09 +0700

    Task 5 ready

--- catalog/models.py
+++ catalog/models.py
@@ -6,29 +6,13 @@ NULLABLE = {'blank': True, 'null': True}
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
-    photo = models.ImageField(upload_to='images/', **NULLABLE)
-    category = models.ForeignKey('catalog.Category', on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='image/', **NULLABLE)
     price = models.FloatField(verbose_name='Цена')
-    date_org = models.DateField(null=True, auto_now=True, verbose_name='Дата создания')
-    date_fill = models.DateField(null=True)
-
-    class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name', 'description', ]
-
-    def __str__(self):
-        return self.name
+    date_org = models.DateField(verbose_name='Дата создания')
+    date_fill = models.DateField(verbose_name='Дата заполнения')
+    category_id = models.ForeignKey('catalog.Category', verbose_name='Категория', on_delete=models.CASCADE)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Категория')
     description = models.TextField(verbose_name='Описание')
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['name', ]
-
-    def __str__(self):
-        return self.name
